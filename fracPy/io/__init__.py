de0cdc8b48f4e9c6954728bdc944661e1302ca68
Dirk Boonzajer <dbs660@vu.nl>
2020-04-11 12:25:57 +0200

    Add get_example_data_folder

---
+++ fracPy/io/__init__.py
@@ -0,0 +1,12 @@
+from fracPy.io import readHdf5
+from pathlib import Path
+
+def get_example_data_folder():
+    """
+    Returns a Path with the example data folder.
+    """
+    fracPy_folder = Path(__file__).parent.parent
+    return fracPy_folder / 'example_data'
+
+if __name__ == '__main__':
+    print(get_example_data_folder())
