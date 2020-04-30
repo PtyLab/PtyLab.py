32a44887cf0b9a7a08bc8a61a92e4923b373c9b2
Daniel Santiago <dannielsantiago@gmail.com>
2020-04-30 16:38:00 +0200

    Adds fresnel propagator singlemode

--- fracPy/physics/propagatorsSinglemode.py
+++ fracPy/physics/propagatorsSinglemode.py
@@ -1,8 +1,25 @@
 import numpy as np
+from .fft import fft2c, ifft2c
 
+def fresnelPropagator(u,z,wavelenght,L):
+    k = 2 * np.pi /wavelenght
+    #source coordinates, this assumes that the field is NxN pixels
+    N = u.shape[-1]
+    dx = L / N
+    x = np.arange(-N / 2, N / 2) * dx
+    [Y, X] = np.meshgrid(x,x)
 
-def fresnelPropagator(*args, **kwargs):
-    raise NotImplementedError
+    #target coordinates
+    dq = wavelenght *z / L;
+    q = np.arange(-N / 2, N / 2) * dq
+    [Qy, Qx] = np.meshgrid(q,q);
+
+    #phase terms
+    Qin = np.exp(1j * k / (2 * z) * (np.square(X) + np.square(Y)) )
+    #Qout = exp(1i * k / (2 * z) * (Qx. ^ 2 + Qy. ^ 2));
+    #r = Qout. * fft2c(Qin. * u)
+    r = fft2c(Qin * u)
+    return r, dq, q, Qx, Qy
 
 def angularSpectrumPropagator():
     raise NotImplementedError
