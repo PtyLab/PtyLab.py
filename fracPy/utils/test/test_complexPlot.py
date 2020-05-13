2174eb4e10737293f002e08f41fda3be12f555c4
Maisie <m.du@arcnl.nl>
2020-05-14 00:07:39 +0200

    wrote an test for complex plots

---
+++ fracPy/utils/test/test_complexPlot.py
@@ -0,0 +1,17 @@
+import unittest
+import numpy as np
+import matplotlib.pyplot as plt
+from fracPy.utils.visualisation import complex_to_rgb, complex_plot
+
+
+
+class TestComplexPlot(unittest.TestCase):
+    def test_complex_plot(self):
+        testComplexArray = np.ones((100,100))*(1+1j)
+        testRGBArray = complex_to_rgb(testComplexArray)
+        complex_plot(testRGBArray)
+        plt.show()
+
+
+if __name__ == '__main__':
+    unittest.main()
