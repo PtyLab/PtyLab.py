Fixed the error calculation bug

example_scripts/preprocessWFS.py
@@ -69,6 +69,7 @@ encoder = (T-T[0]) * 1e-6
 ## read data and correct for darks
 # number of frames is calculated automatically
 framesList = glob.glob('*'+'.tif')
+# always check if the names are sorted properly
 framesList.sort()
 numFrames = len(framesList)-1
 
@@ -82,12 +83,15 @@ ptychogram = np.zeros((numFrames, P//binningFactor, P//binningFactor), dtype=np.
 pbar = tqdm.trange(numFrames, leave=True)
 for k in pbar:
     # get file name
-    pbar.set_description('reading frame' + framesList[k])
-    I = posit(imageio.imread(framesList[k]).astype('float32')-dark-backgroundOffset)
+    # pbar.set_description('reading frame' + framesList[k])
+    # I = posit(imageio.imread(framesList[k]).astype('float32')-dark-backgroundOffset)
+    frameName = str(k)+'.tif'
+    pbar.set_description('reading frame' + frameName)
+    I = posit(imageio.imread(frameName).astype('float32')-dark-backgroundOffset)
     ptychogram[k] = fraccircshift(I, [-detectorShifts[k, 0], detectorShifts[k, 1]])
-    hsvplot(I-ptychogram[k])
-    plt.show()
-    print(k)
+    # hsvplot(I-ptychogram[k])
+    # plt.show()
+    # print(k)
 
 
 
@@ -95,7 +99,7 @@ for k in pbar:
 if N < P:
     # center             
     x = np.arange(P)
-    [X,Y] = np.meshgrid(x, x)
+    [X, Y] = np.meshgrid(x, x)
     # ptychogram_forCenter = ptychogram
     ptychogram_forCenter = posit(ptychogram-300)
     ptychogram_sum = np.sum(ptychogram_forCenter, axis=0)

fracPy/engines/BaseReconstructor.py
@@ -95,6 +95,8 @@ class BaseReconstructor(object):
         self._setObjectProbeROI()
         self._showInitialGuesses()
 
+
+
     def _initializeErrors(self):
         """
         initialize all kinds of errors:
@@ -130,8 +132,9 @@ class BaseReconstructor(object):
         else:
             self.probePowerCorrection = np.sqrt(
                 np.max(np.sum(self.experimentalData.ptychogramDownsampled, (-1, -2))))
-        self.optimizable.probe = self.optimizable.probe/np.sqrt(
-            np.sum(self.optimizable.probe*self.optimizable.probe.conj()))*self.probePowerCorrection
+        if self.probePowerCorrectionSwitch:
+            self.optimizable.probe = self.optimizable.probe/np.sqrt(
+                np.sum(self.optimizable.probe*self.optimizable.probe.conj()))*self.probePowerCorrection
 
     def _probeWindow(self):
         # absorbing probe boundary: filter probe with super-gaussian window function
@@ -429,39 +432,18 @@ class BaseReconstructor(object):
         matches getErrorMetrics.m
         :return:
         """
-        # TODO: Check this, I don't think that it makes sense.
-        # Old code:
-        # if not self.saveMemory:
-        #     # Calculate mean error for all positions (make separate function for all of that)
-        #     if self.FourierMaskSwitch:
-        #         self.errorAtPos = np.sum(np.abs(self.detectorError) * self.W)
-        #     else:
-        #         self.errorAtPos = np.sum(np.abs(self.detectorError))
-        # #print(self.errorAtPos, self.energyAtPos)
-        # self.errorAtPos /= (self.energyAtPos + 1)
-        # print(self.errorAtPos)
-        # eAverage = np.sum(self.errorAtPos)
-        #
-        # # append to error vector (for plotting error as function of iteration)
-        # self.optimizable.error = np.append(self.optimizable.error, eAverage)
-
-
-        # new code:
-        xp = getArrayModule(self.detectorError)
         if not self.saveMemory:
             # Calculate mean error for all positions (make separate function for all of that)
             if self.FourierMaskSwitch:
-                self.errorAtPos = np.sum(np.abs(self.detectorError) * self.W) / xp.asarray(1+self.energyAtPos)
+                self.errorAtPos = np.sum(np.abs(self.detectorError) * self.W, axis=(-1, -2))
             else:
-                self.errorAtPos = np.sum(np.abs(self.detectorError)) / xp.asarray(1+self.energyAtPos)
-        #print(self.errorAtPos, self.energyAtPos)
-        #self.errorAtPos /= (self.energyAtPos + 1)
-        #print(self.errorAtPos)
-        eAverage = asNumpyArray(xp.sum(self.errorAtPos))
-
+                self.errorAtPos = np.sum(np.abs(self.detectorError), axis=(-1, -2))
+        self.errorAtPos = asNumpyArray(self.errorAtPos)/asNumpyArray(self.energyAtPos + 1)
+        eAverage = np.sum(self.errorAtPos)
 
         # append to error vector (for plotting error as function of iteration)
         self.optimizable.error = np.append(self.optimizable.error, eAverage)
+
 
     def getRMSD(self, positionIndex):
         """