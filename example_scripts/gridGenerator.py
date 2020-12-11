87bd0efcfb5c99acd0b61a8ef7de453cb0dacf96
MaisieD <m.du@arcnl.nl>
2020-12-11 15:51:31 +0100

    manually added the gridGenerator and tsp_ga

---
+++ example_scripts/gridGenerator.py
@@ -0,0 +1,64 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+from numpy import vectorize
+from fracPy.utils.scanGrids import GenerateRasterGrid, GenerateConcentricGrid, GenerateNonUniformFermat
+from fracPy.utils.scanGrids import tsp_ga
+
+
+## generate(non - optimal) grid
+
+numPoints = 100   # number of points
+radius = 100    # radius of final scan grid (in pixels)
+p = 1    # p = 1 is standard Fermat;  p > 1 yields more points towards the center of grid
+# first argument: number of points, second argument: scaling of Fermat grid
+R, C = GenerateNonUniformFermat(numPoints, radius=radius, power=p)
+
+xy = np.vstack((R, C)).T  # convert to an (n,2) array
+n = len(R)  # number of points
+distance = np.sqrt((R[0] - R[-1]) ** 2 + (C[1] - C[-1]) ** 2)
+for k in np.arange(1, n):
+    distance = distance + np.sqrt((R[k] - R[k - 1]) ** 2 + (C[k] - C[k - 1]) ** 2)
+print('initial travel distance: %f' %distance)
+
+# show scan grid
+plt.figure(figsize=(5, 5), num=99)
+plt.plot(R, C, 'o-')
+plt.xlabel('um')
+plt.title('initial scan grid')
+plt.show(block=False)
+
+## traveling salesman problem, genetic algorithm(tsp_ga)
+numIteration = 5e3
+# g = tsp_ga(hash_map=dist_dict, xy=xy, meanDist=meanDist, start='0', population_size=40, iterations=numIteration)
+optRoute = tsp_ga(xy=xy, population_size=40, iterations=numIteration).converge()
+Rnew = xy[optRoute, 0]
+Cnew = xy[optRoute, 1]
+
+
+## show optimization result
+plt.figure(figsize=(5, 5), num=100)
+plt.plot(Rnew, Cnew, 'o-')
+plt.xlabel('um')
+plt.ylabel('um')
+plt.title('optimized scan grid')
+plt.show(block=False)
+
+averageDistance = np.sum(np.sqrt(np.diff(R[optRoute]) ** 2 + np.diff(C[optRoute]) ** 2)) / len(optRoute)
+print('average step size: %i um' %averageDistance)
+print('number of scan points: '+ str(len(optRoute)))
+
+## generate txt file
+
+# ColsForTXT = [R(optRoute) C(optRoute)]
+#
+#
+# fileID = fopen('positions.txt', 'w') # open
+# file
+# for writing('w')
+#     fprintf(fileID, '#12s #4u\r\n', 'number of positions: ', size(ColsForTXT, 1))
+# fprintf(fileID, '#12s #12s\r\n', 'y (row) [um] |', 'x (col) [um]')
+# fprintf(fileID, '#4.2f #4.2f\r\n', ColsForTXT)
+# fclose(fileID)
+
+
--- fracPy/utils/scanGrids.py
+++ fracPy/utils/scanGrids.py
@@ -1,4 +1,8 @@
 import numpy as np
+from numpy import vectorize
+import random
+from matplotlib import pyplot as plt
+
 
 def GenerateNonUniformFermat(n, radius=1000, power=1):
     """
@@ -83,6 +87,185 @@ def GenerateRasterGrid(n, ds, randomOffset = False, amplitude = 1):
 
 
 
+class tsp_ga():
+    """
+    genetic algorithm for traveling salesman problem.
+    """
+    def __init__(self, xy, start='0', population_size=5, iterations=100):
+        self.xy = xy
+        self.start = start
+        self.population_size = population_size
+        self.iterations = int(iterations)
+
+        self.figure = plt.figure(num=999, clear=True, figsize=(5, 5))
+        self.ax_scanGridOpt = self.figure.add_subplot(111)
+        self.ax_scanGridOpt.set_title('optimized scan grid')
+        self.ax_scanGridOpt.set_xlabel('um')
+        self.ax_scanGridOpt.set_ylabel('um')
+        self.ax_scanGridOpt_plot, = self.ax_scanGridOpt.plot(xy[:, 0], xy[:, 1], 'o-')
+        self.figure.show()
+
+        n = self.xy.shape[0]
+        numEl = np.sum(x for x in range(n))
+        cities_ = np.linspace(0, n - 1, n).astype(int)
+        cities = ["" for i in range(cities_.size)]
+        for i in range(cities_.size):
+            cities[i] = np.array2string(cities_[i])
+
+        edges = []
+        totalDist = 0
+        dist_dict = {c: {} for c in cities}
+        for idx_1 in range(0, len(cities) - 1):
+            for idx_2 in range(idx_1 + 1, len(cities)):
+                city_a = cities[idx_1]  # xy[idx_1,0].astype(int)
+                city_b = cities[idx_2]  # xy[idx_2,0].astype(int)
+                # dist = get_distance(xy[int(city_a), 0], xy[int(city_a), 1], xy[int(city_b), 0], xy[int(city_b), 1], )
+                dist = np.sqrt((xy[int(city_a), 0] - xy[int(city_b), 0]) ** 2 +
+                               (xy[int(city_a), 1] - xy[int(city_b), 1]) ** 2)
+                totalDist += dist
+                dist_dict[city_a][city_b] = dist
+                edges.append((city_a, city_b, dist))
+
+        self.meanDist = totalDist / numEl
+        self.hash_map = dist_dict
+        self.cities = [k for k in self.hash_map.keys()]
+        self.cities.remove(start)
+        self.genes = []
+        self.generate_genes = vectorize(self.generate_genes)
+        self.generate_genes()
+
+    def generate_genes(self):
+        for i in range(self.population_size):
+            gene = [self.start]
+            city_a = '0'
+            options = [k for k in self.cities]
+            loop = 0
+            threshold = len(self.cities)
+            while len(gene) < len(self.cities) + 1:
+                city_b = random.choice(options)
+                try:
+                    dist = self.hash_map[city_a][city_b]
+                except:
+                    dist = self.hash_map[city_b][city_a]
+                if (dist > self.meanDist/4) & (loop < threshold):
+                    loop += 1
+                    continue
+                if (dist > self.meanDist/3) & (loop < 2*threshold):
+                    loop += 1
+                    continue
+                if (dist > self.meanDist/2) & (loop < 3*threshold):
+                    loop += 1
+                    continue
+                if (dist > self.meanDist) & (loop < 4*threshold):
+                    loop += 1
+                    continue
+                loc = options.index(city_b)
+                loop = 0
+                gene.append(city_b)
+                del options[loc]
+                city_a = city_b
+            gene.append(self.start)
+            self.genes.append(gene)
+        # return self.genes
+
+    def GA_Matlab(self):
+        globalMin = np.Inf
+        distHistory = np.zeros((self.iterations, 1))
+        tmpPop = []
+        for i in range(4):
+            tmpPop.append(self.genes[i])
+        newPop = []
+
+        counter = 0
+        for iter in range(self.iterations):
+            counter = counter + 1
+            totalDist = []
+        # Evaluate Each Population Member(Calculate Total Distance)
+            for geneP in self.genes: #range(self.population_size):
+                d = 0  #self.hash_map[geneP[-1]][geneP[0]] # Closed Path
+                for k in range(1,len(geneP)):
+                    try:
+                        city_a = geneP[k-1]
+                        city_b = geneP[k]
+                        dist = self.hash_map[city_a][city_b]
+                    except:
+                        dist = self.hash_map[city_b][city_a]
+                    d += dist
+                totalDist.append(d)
+
+        #  Find the Best Route in the Population
+            if iter == 0:
+                minDist = totalDist[0]
+                index = 0
+                distHistory[iter] = minDist
+            else:
+                minDist = np.amin(totalDist)
+                index = np.argmin(totalDist)
+                distHistory[iter] = minDist
+            if minDist < globalMin:
+                globalMin = minDist
+                optRoute = self.genes[index]
+            del newPop
+            newPop = []
+            # Genetic Algorithm Operators
+            randomOrder = np.random.permutation(self.population_size)
+            for p in range(4, self.population_size+1,4):
+                randomOrderP = randomOrder[p-4:p]
+                rtes = []
+                dists = []
+                for ii in range(4):
+                    randomOrderP2 = randomOrderP[ii]
+                    rtes.append(self.genes[randomOrderP2])
+                    dists.append(totalDist[randomOrderP2])
+                idx = np.argmin(dists)
+                bestOf4Route = rtes[idx]
+                routeInsertionPoints = np.transpose(np.sort(np.ceil((len(geneP) - 2) * np.random.rand(1, 2))))
+                I = int(routeInsertionPoints[0])
+                J = int(routeInsertionPoints[1])
+                for k in range(4):  # Mutate the Best to get Three New Routes
+                    tmpPop[k] = bestOf4Route.copy()
+                    if k == 1:  # Flip
+                        index_a = []
+                        for ii in range(I,J+1):
+                            index_a.append(tmpPop[k][J+I-ii])
+                        jj = 0
+                        for ii in range(I,J+1):
+                            tmpPop[k][ii] = index_a[jj]
+                            jj += 1
+                    elif k == 2:  # Swap
+                        index_a = tmpPop[k][I]
+                        index_b = tmpPop[k][J]
+                        tmpPop[k][I] = index_b
+                        tmpPop[k][J] = index_a
+                    elif k == 3:  # Slide
+                        index_a = []
+                        for ii in range(I+1,J+1):
+                            index_a.append(tmpPop[k][ii])
+                        index_b = tmpPop[k][I]
+                        jj = 0
+                        for ii in range(I , J):
+                            tmpPop[k][ii] = index_a[jj]
+                            jj += 1
+                        tmpPop[k][J] = index_b
+                    newPop.append(tmpPop[k])
+            if iter % (self.iterations//10) == 0:
+                print('distance: %i um, numIteration: %i, ' %(globalMin,iter))
+                current_best_gene_r = np.array(optRoute).astype(int)
+
+                self.ax_scanGridOpt_plot.set_data(self.xy[current_best_gene_r, 0],
+                                                  self.xy[current_best_gene_r, 1])
+                self.figure.canvas.draw()
+                self.figure.canvas.flush_events()
+
+            del self.genes
+            self.genes = newPop.copy()
+        return globalMin, optRoute
+
+    def converge(self):
+        values = self.GA_Matlab()
+        current_score = values[0]
+        current_best_gene = values[1]
+        return np.array(current_best_gene).astype(int)
 
 
 
