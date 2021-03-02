d5cd019be924979d78d25539276f67305728ae50
Anne <a.d.beurs@arcnl.nl>
2021-03-02 19:00:52 +0100

    wrote the main transform functions

--- fracPy/engines/aPIE.py
+++ fracPy/engines/aPIE.py
@@ -24,6 +24,7 @@ from fracPy.operators.operators import aspw
 import logging
 import sys
 
+
 class aPIE(BaseReconstructor):
     """
     aPIE: angle correction PIE: ePIE combined with Luus-Jaakola algorithm (the latter for angle correction) + momentum
@@ -37,7 +38,6 @@ class aPIE(BaseReconstructor):
         self.logger.info('Sucesfully created aPIE aPIE_engine')
         self.logger.info('Wavelength attribute: %s', self.optimizable.wavelength)
         self.initializeReconstructionParams()
-
 
     def initializeReconstructionParams(self):
         """
@@ -74,7 +74,8 @@ class aPIE(BaseReconstructor):
         self.pbar = tqdm.trange(self.numIterations, desc='aPIE', file=sys.stdout, leave=True)
         for loop in self.pbar:
             # save theta search history
-            self.optimizable.thetaHistory = np.append(self.optimizable.thetaHistory, asNumpyArray(self.optimizable.theta))
+            self.optimizable.thetaHistory = np.append(self.optimizable.thetaHistory,
+                                                      asNumpyArray(self.optimizable.theta))
 
             # select two angles (todo check if three angles behave better)
             theta = np.array([self.optimizable.theta, self.optimizable.theta + thetaSearchRadiusList[loop] *
@@ -85,10 +86,11 @@ class aPIE(BaseReconstructor):
             objectTemp = self.optimizable.object.copy()
 
             # probe and object buffer (todo maybe there's more elegant way )
-            probeBuffer = xp.zeros_like(probeTemp) # shape=(np.array([probeTemp, probeTemp])).shape)
+            probeBuffer = xp.zeros_like(probeTemp)  # shape=(np.array([probeTemp, probeTemp])).shape)
             probeBuffer = [probeBuffer, probeBuffer]
-            objectBuffer = xp.zeros_like(objectTemp) #, shape=(np.array([objectTemp, objectTemp])).shape)  # for polychromatic case this will need to be multimode
-            objectBuffer = [objectBuffer,objectBuffer]
+            objectBuffer = xp.zeros_like(
+                objectTemp)  # , shape=(np.array([objectTemp, objectTemp])).shape)  # for polychromatic case this will need to be multimode
+            objectBuffer = [objectBuffer, objectBuffer]
             # initialize error
             errorTemp = np.zeros((2, 1))
 
@@ -96,7 +98,8 @@ class aPIE(BaseReconstructor):
                 self.optimizable.probe = probeTemp
                 self.optimizable.object = objectTemp
                 # reset ptychogram (transform into estimate coordinates)
-                Xq = T_inv(self.optimizable.Xd, self.optimizable.Yd, self.optimizable.zo, theta[k]) # todo check if 1D is enough to save time
+                Xq = T_inv(self.optimizable.Xd, self.optimizable.Yd, self.optimizable.zo,
+                           theta[k])  # todo check if 1D is enough to save time
                 for l in range(self.experimentalData.numFrames):
                     temp = self.ptychogramUntransformed[l]
                     f = interp2d(self.optimizable.xd, self.optimizable.xd, temp, kind='linear', fill_value=0)
@@ -110,12 +113,12 @@ class aPIE(BaseReconstructor):
                     self.experimentalData.ptychogram) * np.linalg.norm(self.ptychogramUntransformed)
 
                 self.experimentalData.W = np.ones_like(self.optimizable.Xd)
-                fw = interp2d(self.optimizable.xd, self.optimizable.xd, self.experimentalData.W, kind='linear', fill_value=0)
+                fw = interp2d(self.optimizable.xd, self.optimizable.xd, self.experimentalData.W, kind='linear',
+                              fill_value=0)
                 self.experimentalData.W = abs(fw(Xq[0], self.optimizable.xd))
                 self.experimentalData.W = np.nan_to_num(self.experimentalData.W)
                 self.experimentalData.W[self.experimentalData.W == 0] = 1e-3
                 self.experimentalData.W = xp.array(self.optimizable.W)
-
 
                 # todo check if it is right
                 if self.params.fftshiftSwitch:
@@ -149,7 +152,6 @@ class aPIE(BaseReconstructor):
                     # probe update
                     self.optimizable.probe = self.probeUpdate(objectPatch, DELTA)
 
-
                 # get error metric
                 self.getErrorMetrics()
                 # remove error from error history
@@ -178,7 +180,8 @@ class aPIE(BaseReconstructor):
             self.optimizable.thetaMomentum = self.feedback * dtheta + self.aPIEfriction * self.optimizable.thetaMomentum
             # print updated theta
             self.pbar.set_description('aPIE: update a=%.3f deg (search radius=%.3f deg, thetaMomentum=%.3f deg)'
-                                      % (self.optimizable.theta, thetaSearchRadiusList[loop], self.optimizable.thetaMomentum))
+                                      % (self.optimizable.theta, thetaSearchRadiusList[loop],
+                                         self.optimizable.thetaMomentum))
 
             # show reconstruction
             if loop == 0:
@@ -213,7 +216,6 @@ class aPIE(BaseReconstructor):
 
         # self.thetaSearchRadiusMax = thetaSearchRadiusList[loop]
 
-
     def objectPatchUpdate(self, objectPatch: np.ndarray, DELTA: np.ndarray):
         """
         Todo add docstring
@@ -240,34 +242,69 @@ class aPIE(BaseReconstructor):
         r = self.optimizable.probe + self.betaProbe * xp.sum(frac * DELTA, axis=(0, 1, 3), keepdims=True)
         return r
 
+    def xtotiltU(self, x: np.ndarray, y: np.ndarray, theta: np.ndarray, wavelength: np.ndarray):
+        """
+        go from detector plane coordinates to corresponding  tilted plane spatial frequency coordinates
+        :param x: detector x coordinates
+        :param y: detector y coordinates
+        :param theta: tilt angle between sample plane and detector plane in degrees
+        :params wavelength: illumination wavelength
+        :return: warped spatial frequencies u,v associated with x,y
+        """
+        theta = toRadians(theta)
 
-    def xtotiltU(self, x, y, theta):
-        """
-        Coordinate transformation from detector coordinates to coordinates to spatial frequencies of the object, in the case
-        that the object plane is tilted with respect to the detector plane
-        :param x,y: Detector coordinates
-        :param theta: tilt angle between sample
-        :return:
-        """
-        r0 = np.sqrt(x ** 2 + y ** 2 + self.zo ** 2)
+        ro = np.sqrt(x ** 2 + y ** 2 + self.experimentalData.zo ** 2)
         v = y
-        u = x * np.cos(toDegree(theta)) - np.sin(toDegree(theta)) * (r0 - self.zo)
-        return u,v
+        u = (x * np.cos(theta) - np.sin(theta) * (ro - self.experimentalData.zo)) / (wavelength * ro)
 
+        return u, v
 
-def T_inv(xd, yd, z, theta):
-    """
-    inverse coordinate transformation
-    """
-    if theta != 45:
-        rootTerm = np.sqrt((z * np.cos(toDegree(theta))) ** 2 + xd ** 2 + yd ** 2 * np.cos(toDegree(2 * theta)) -
-                           2 * xd * z * np.sin(toDegree(theta)))
-        x = (xd * np.cos(toDegree(theta)) - z * np.sin(toDegree(theta)) * np.cos(toDegree(theta)) +
-             np.sin(toDegree(theta)) * rootTerm) / np.cos(toDegree(2 * theta))
-    else:
-        x = (xd ** 2 - (yd ** 2) / 2 - xd * np.sqrt(2) * z) / (xd * np.sqrt(2) - z)
-    return x
+    def tiltUtoX(self, u: np.ndarray, v: np.ndarray, theta: np.ndarray, wavelength: np.ndarray):
+        """
+        go from tilted plane spatial frequency coordinates to corresponding detector plane coordinates
+        :param u: spatial frequency coordinates associated with x in sample coordinates that are tilted with respect to
+        detector coordinates
+        :param v: spatial frequency coordinates associated with y in sample coordinates that are tilted with respect to detector
+        coordinates
+        :param theta: tilt angle between sample plane and detector plane in degrees
+        :param wavelength:illumination wavelength
+        :return:
+        :rtype: x,y(the detector coordinates associated with the input spatial frequencies, after transform inversion)
+        """
+        # for derivation see ~placeholder for pdf
+        theta = toRadians(theta)
+        a = 1. + ((u / v) + np.sin(theta) / (v * wavelength) / np.cos(theta)) ** 2 - (v * wavelength) ** (-2)
+        b = -(2. * np.sin(theta) * self.experimentalData.zo / (v * np.cos(theta)) ** 2) * (u + np.sin(theta)) / \
+            wavelength
+        c = (1 + np.tan(theta))
+        y = (-b - np.sign(v) * np.sqrt(b ** 2 - 4 * a * c)) / (2 * a)
+        # y is not invertible with this quadratic equation at v=0 as 0/0 is undefined, but y=lambda*r0*v, so y=0
+        y = np.where(v == 0, y, 0)
+        # at a=0 we again gets something that is not defined, furthermore, the numerical accuracy due to rounding
+        # error s becomes problematic when a=very small, so we replace by the linear equation for those cases.
+        y = np.where(a < 1.e-6, y, -c / b)
+        # for x its less trivial to choose the right sign for the solution,
+        # calculate the spatial frequencies where x=0, this marks a ux0(y) line. all coordinates that have a u(x,
+        # y) that is larger are positive, all that are smaller have a negative x
+        #
 
+        rx0 = np.sqrt(self.experimentalData.zo ** 2 + y ** 2)
+        ux0 = np.sin(theta) * (self.experimentalData.zo - rx0) / (wavelength * rx0)
+        x = np.sign(u - ux0) * np.real(np.sqrt(((v * wavelength) ** (-2) - 1) * y ** 2 - self.zo ** 2))
+        # when y=0,v=0 this equation is not defined so we invert for y=0, and get another quadratic equation for x
+        ax = (np.cos(2 * theta) - 2 * wavelength * u * np.sin(theta) - (wavelength * u) ** 2)
+        bx = 2 * self.experimentalData.zo * np.sin(theta) * np.cos(theta)
+        self.experimentalData.zo = self.experimentalData.zo
+        cx = -(self.experimentalData.zo ** 2 * ((wavelength * u) * (2 * np.sin(theta) + (wavelength * u))))
+        x2 = -bx + np.sqrt(bx ** 2 - 4 * ax * cx) / (2 * ax)
+        x2 = np.where(np.abs(ax) < 1.e-6, -cx / bx, x2)
+        x = np.where(np.abs(y) < 1.e-6, x2, y)
+        x = np.where(np.abs(u - ux0) < 1.e-6, 0, x)
+        return x, y
+    def xtoU(self,x,y,wavelength):
+        r0 = np.sqrt(x ** 2 + y ** 2 + self.experimentalData.zo ** 2)
+        
 
-def toDegree(theta: Any) -> float:
-    return np.pi * theta / 180
+def toRadians(theta):
+    theta =theta*np.pi/180.
+    return theta
