734c48aae26bf101b186a0691348fe0ad5eec077
MaisieD <m.du@arcnl.nl>
2021-04-15 11:39:25 +0200

    now works after refactoring

--- fracPy/Engines/__init__.py
+++ fracPy/Engines/__init__.py
@@ -1,5 +1,5 @@
 #from . import ePIE_reconstructor, mPIE_reconstructor, pSD_reconstructor
-# engines available by default
+# Engines available by default
 from .ePIE_reconstructor import ePIE
 from .pSD_reconstructor import pSD
 from .mPIE_reconstructor import mPIE
@@ -8,7 +8,7 @@ from .mqNewton_reconstructor import mqNewton
 from .aPIE import aPIE
 from .multiPIE_reconstructor import multiPIE
 from .zPIE_reconstructor import zPIE
-# for other engines (like one you are developing but which is too specific) you can always import fracPy.engines.<your_engine_filename>.<your_class>
+# for other Engines (like one you are developing but which is too specific) you can always import fracPy.Engines.<your_engine_filename>.<your_class>
 from .BaseReconstructor import BaseReconstructor
 
 
