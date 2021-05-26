b23fe977aba9d064aee1c7f940293668df8d6985
MaisieD <m.du@arcnl.nl>
2021-05-26 10:34:42 +0200

    Updated CPSC implementation

--- fracPy/Params/ReconstructionParameters.py
+++ fracPy/Params/ReconstructionParameters.py
@@ -10,23 +10,23 @@ class Reconstruction_parameters(object):
     optimizer1 = optimizers.
     """
     def __init__(self):
-
-
         # datalogger
         self.logger = logging.getLogger('Params')
-        
+
         # Default settings for switches, settings that involve how things are computed
         self.fftshiftSwitch = False
         self.fftshiftFlag = False
         self.FourierMaskSwitch = False
         self.CPSCswitch = False
+        self.CPSCupsamplingFactor = None
+
         self.fontSize = 17
         self.intensityConstraint = 'standard'  # standard or sigmoid
         self.propagator = 'Fraunhofer'  # 'Fresnel' 'ASP'
-        self.momentumAcceleration = False  # default False, it is turned on in the individual Engines that use momentum
-        self.adaptiveMomentumAcceleration = False # default False, it is turned on in the individual Engines that use momentum
+        self.momentumAcceleration = False  # default False, it is turned on in the individual engines that use momentum
+        self.adaptiveMomentumAcceleration = False  # default False, it is turned on in the individual engines that use momentum
 
-        ## Specific reconstruction settings that are the same for all Engines
+        ## Specific reconstruction settings that are the same for all engines
         self.gpuSwitch = False
         # This only makes sense on a GPU, not there yet
         self.saveMemory = False
@@ -45,17 +45,17 @@ class Reconstruction_parameters(object):
         self.absObjectBeta = 1e-2  # relaxation parameter for abs-only constraint
         self.objectContrastSwitch = False  # pushes object to zero outside ROI
         # probe regularization
-        self.probeSmoothenessSwitch = False # enforce probe smootheness
+        self.probeSmoothenessSwitch = False  # enforce probe smootheness
         self.probeSmoothnessAleph = 5e-2  # relaxation parameter for probe smootheness
         self.probeSmoothenessWidth = 3  # loose object support diameter
-        self.probeBoundary = False # probe cut-off based on a window
+        self.probeBoundary = False  # probe cut-off based on a window
         self.absorbingProbeBoundary = False  # controls if probe has period boundary conditions (zero)
         self.absorbingProbeBoundaryAleph = 5e-2
         self.probePowerCorrectionSwitch = False  # probe normalization to measured PSD
         self.modulusEnforcedProbeSwitch = False  # enforce empty beam
         self.comStabilizationSwitch = False  # center of mass stabilization for probe
         self.absProbeSwitch = False  # force the probe to be abs-only
-        self.absProbeBeta = 1e-2   # relaxation parameter for abs-only constraint
+        self.absProbeBeta = 1e-2  # relaxation parameter for abs-only constraint
         # other
         self.couplingSwitch = False  # couple adjacent wavelengths
         self.couplingAleph = 50e-2  # couple adjacent wavelengths (relaxation parameter)
@@ -67,10 +67,9 @@ class Reconstruction_parameters(object):
         self.PSDestimationSwitch = False
         self.objectContrastSwitch = False  # pushes object to zero outside ROI
         self.positionCorrectionSwitch = False  # position correction for encoder
-        self.adaptiveDenoisingSwitch = False # estimated noise floor to be clipped from raw data
+        self.adaptiveDenoisingSwitch = False  # estimated noise floor to be clipped from raw data
 
-        self.CPSCswitch = False
-        self.upsamplingFactor = None
+
 
 
 
