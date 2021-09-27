7962003d7d79cc88c295096334a1ac52a74fa64a
Dirk Boonzajer @lab <boonzajer@posteo.net>
2021-09-27 14:54:33 +0200

    Separate aspw in a part that is cached and a part that is not

--- fracPy/Operators/test_Operators.py
+++ fracPy/Operators/test_Operators.py
@@ -1,6 +1,43 @@
 from numpy.testing import assert_allclose
 
 from fracPy import Operators, easyInitialize
+import numpy as np
+from numpy.testing import assert_allclose
+import time
+
+def test_caching_aspw():
+    try:
+        import cupy as cp
+        xp = cp
+    except ImportError:
+        xp = np
+    E = xp.random.rand(10,1,3, 512,512)
+    z = 1e-3
+    wl = 512e-9
+    pixel_pitch = 10e-6
+    L = pixel_pitch*E.shape[-1]
+
+    from cupyx import time as timer
+    # run this to warm up the GPU
+    timer.repeat(Operators.Operators.aspw, (E, z, wl, L),{}, n_repeat=200, n_warmup=50)
+
+    t0 = time.time()
+    for i in range(100):
+        E_prop = Operators.Operators.aspw_cached(E, z, wl, L)
+    if xp is not np:
+        E_prop = xp.asnumpy(E_prop)
+    t1 = time.time()
+    t_cached = t1 - t0
+    for i in range(100):
+        E_prop2 = Operators.Operators.aspw(E, z, wl, L)[0]
+    if xp is not np:
+        E_prop2 = E_prop2.get()
+    t2 = time.time()
+    t_noncached = t2-t1
+    print(f'\n\nNon-cached took: {t_noncached}', f'Cached took {t_cached}s')
+    #E_prop = np.squeeze(E_prop)
+
+    assert_allclose(E_prop, E_prop2)
 
 
 def test_object2detector():
@@ -8,7 +45,6 @@ def test_object2detector():
     params.gpuSwitch = True
     reconstruction._move_data_to_gpu()
     _doit(reconstruction, params)
-
 
     params.gpuSwitch = False
     reconstruction._move_data_to_cpu()
@@ -36,7 +72,7 @@ def test__propagate_fresnel():
     reconstruction.initializeObjectProbe()
     reconstruction.esw = 2
     for operator in [Operators.Operators.propagate_fresnel,
-                      Operators.Operators.propagate_ASP,
+                     Operators.Operators.propagate_ASP,
                      Operators.Operators.propagate_scaledASP,
                      Operators.Operators.propagate_twoStepPolychrome,
                      Operators.Operators.propagate_scaledPolychromeASP]:
@@ -48,7 +84,7 @@ def test__propagate_fresnel():
             t0 = time.time()
             out = operator(reconstruction.probe, params, reconstruction)
             t1 = time.time()
-            print(i, 1e3*(t1-t0), 'ms')
+            print(i, 1e3 * (t1 - t0), 'ms')
 
         params.gpuSwitch = False
         reconstruction._move_data_to_cpu()
@@ -58,5 +94,8 @@ def test__propagate_fresnel():
             t0 = time.time()
             out = operator(reconstruction.probe, params, reconstruction)
             t1 = time.time()
-            print(i, 1e3*(t1-t0), 'ms')
+            print(i, 1e3 * (t1 - t0), 'ms')
 
+
+def test_aspw_cached():
+    assert False
