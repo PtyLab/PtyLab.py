f6e5a0e43e9019944360589c5a07434ebf9b24a5
Dirk Boonzajer Flaes <dirkewoud.boonzajerflaes@leibniz-ipht.de>
2022-12-06 12:21:54 +0100

    simulateData now creates the right file.
    Also fixed setOrientation == 0, and adjusted the settings for exampleReconstructionCPM.py such that the reconstruction runs smoothly

--- example_scripts/simulateData.py
+++ example_scripts/simulateData.py
@@ -1,7 +1,8 @@
-# This script contains a minimum working example of how to generate data
-# todo: mention that it generates simu.hdf5
+# This script contains a minimum working example of how to generate data.
+# The data are stored in example_data/simu.hdf5
+
 import numpy as np
-from PtyLab.utils.utils import circ, gaussian2D, cart2pol
+from PtyLab.utils.utils import circ, gaussian2D, cart2pol, fft2c
 from PtyLab.utils.scanGrids import GenerateNonUniformFermat
 from PtyLab.Operators.Operators import aspw
 from PtyLab.utils.visualisation import hsvplot, show3Dslider
@@ -10,7 +11,11 @@ from scipy.signal import convolve2d
 import os
 import h5py
 
-fileName = "simuRecent_copy"
+# turn this off not to export the data at the end
+export_data = True
+from PtyLab.io import getExampleDataFolder
+
+fileName = "simu"
 
 # set physical properties
 
@@ -45,7 +50,7 @@ Xo, Yo = np.meshgrid(xo, xo)
 # generate illumination
 # note: simulate focused beam
 # goal: 1:1 image iris through (low-NA) lens with focal length f onto an object
-f = 5e-3  # focal length of lens, creating a focused probe
+f = 8e-3  # focal length of lens, creating a focused probe
 pinhole = circ(Xp, Yp, Lp / 2)
 pinhole = convolve2d(pinhole, gaussian2D(5, 1).astype(np.float32), mode="same")
 
@@ -78,13 +83,12 @@ b = 33  # topological charge (feel free to play with this number)
 theta, rho = cart2pol(Xo, Yo)
 t = (1 + np.sign(np.sin(b * theta + 2 * np.pi * (rho / d) ** 2))) / 2
 # phaseFun = np.exp(1.j * np.arctan2(Yo, Xo))
-phaseFun = 1
-# phaseFun = np.exp(1.j*( 2* theta + 2*np.pi * (rho/d)**2))
+# phaseFun = 1
+phaseFun = np.exp(1.j*( theta + 2*np.pi * (rho/d)**2))
 t = t * circ(Xo, Yo, Lo) * (1 - circ(Xo, Yo, 200 * dxo)) * phaseFun + circ(
     Xo, Yo, 130 * dxo
 )
 obj = convolve2d(t, gaussian2D(5, 3), mode="same")  # smooth edges
-# obj_phase = np.exp(1.j*2*np.pi/wavelength*(Xo**2+Yo**2)*20)
 object = obj * phaseFun
 
 plt.figure(figsize=(5, 5), num=2)
@@ -97,7 +101,7 @@ plt.show(block=False)
 # generate non-uniform Fermat grid
 # parameters
 numPoints = 100  # number of points
-radius = 100  # radius of final scan grid (in pixels)
+radius = 150  # radius of final scan grid (in pixels)
 p = 1  # p = 1 is standard Fermat;  p > 1 yields more points towards the center of grid
 R, C = GenerateNonUniformFermat(numPoints, radius=radius, power=p)
 # show scan grid
@@ -116,7 +120,7 @@ encoder = np.vstack((R * dxo, C * dxo)).T
 
 # prevent negative indices by centering spiral coordinates on object
 positions = np.round(encoder / dxo)
-offset = 50
+offset = np.array([50, 20])
 positions = (positions + No // 2 - Np // 2 + offset).astype(int)
 
 # get number of positions
@@ -125,30 +129,37 @@ print("generate positions(" + str(numFrames) + ")")
 
 # calculate estimated overlap
 # expected beam size, required to calculate overlap (expect Gaussian-like beam, derive from second moment)
-beamSize = (
-    np.sqrt(np.sum((Xp**2 + Yp**2) * np.abs(probe) ** 2) / np.sum(abs(probe) ** 2))
-    * 2.355
-)
+beamSize = np.sqrt(np.sum((Xp**2 + Yp**2) * np.abs(probe) ** 2) / np.sum(abs(probe) ** 2)) * 2.355
 distances = np.sqrt(np.diff(R) ** 2 + np.diff(C) ** 2) * dxo
-averageDistance = np.mean(distances)
+averageDistance = np.mean(distances)*1e6
 print("average step size:%.1f (um)" % averageDistance)
+print(f'probe diameter: {beamSize*1e6:.2f}')
 print("number of scan points: %d" % numFrames)
 
 # show scan grid on object
 plt.figure(figsize=(5, 5), num=33)
 ax1 = plt.axes()
 hsvplot(np.squeeze(object), ax=ax1)
-ax1.plot(positions[0, 1] + Np // 2, positions[0, 0] + Np // 2, ".")
-ax1.set_title("complex probe")
+
+pos_pix = positions + Np//2
+dia_pix = beamSize / dxo
+ax1.plot(pos_pix[:,-1], pos_pix[:,-2], 'ro', alpha=0.9, )
+ax1.set_xlim(pos_pix[:,1].min()-100, pos_pix[:,1].max()+100)
+ax1.set_ylim(pos_pix[:,0].max()+100, pos_pix[:,0].min()-100)
+# indicate the probe with the typical diameter
+for p in pos_pix:
+    c = plt.Circle(p, radius=dia_pix/2, color='black', fill=False, alpha=0.5)
+    ax1.add_artist(c)
+ax1.set_title("object with probe positions")
 plt.show(block=False)
 
-# set data
+## set data
 entrancePupilDiameter = beamSize
 
-# generate ptychogram
+## generate ptychogram
 ptychogram = np.zeros((numFrames, Nd, Nd))
 
-propagator = "Fraunhofer"
+propagator = "fraunhofer"
 for loop in np.arange(numFrames):
     # get object patch
     row, col = positions[loop]
@@ -159,7 +170,7 @@ for loop in np.arange(numFrames):
     # multiply each probe mode with object patch
     esw = objectPatch * probe
     # generate diffraction data, propagate the esw to the detector plane
-    ESW = np.fft.fftshift(np.fft.fft2(np.fft.ifftshift(esw), norm="ortho"))
+    ESW = fft2c(esw)
     # save data in ptychogram
     ptychogram[loop] = abs(ESW) ** 2
     # inspect diffraction data
@@ -169,24 +180,24 @@ for loop in np.arange(numFrames):
 bitDepth = 14
 maxNumCountsPerDiff = 2**bitDepth
 
-# normalize data (ptychogram)
+## normalize data (ptychogram)
+
 ptychogram = ptychogram / np.max(ptychogram) * maxNumCountsPerDiff
 ptychogram_noNoise = ptychogram.copy()
 
-# simulate Poisson noise
+
+
+## simulate Poisson noise
 noise = np.random.poisson(ptychogram)
 ptychogram += noise
 ptychogram[ptychogram < 0] = 0
 
-# compare noiseless data noisy
+## compare noiseless data noisy
 ptychogram_comparison = np.concatenate((ptychogram_noNoise, ptychogram), axis=1)
 show3Dslider(np.log(ptychogram_comparison + 1))
 
 
-# export data
-export_data = False  # exportBool in MATLAB
-from PtyLab.io import getExampleDataFolder
-
+## export data
 saveFilePath = getExampleDataFolder()
 os.chdir(saveFilePath)
 if export_data:
@@ -200,5 +211,6 @@ if export_data:
     hf.create_dataset("zo", data=(zo,), dtype="f")
     hf.create_dataset("wavelength", data=(wavelength,), dtype="f")
     hf.create_dataset("entrancePupilDiameter", data=(entrancePupilDiameter,), dtype="f")
+    hf.create_dataset('orientation', data=0)
     hf.close()
     print("An hd5f file has been saved")
