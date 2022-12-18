9ca541fcbcb090dbcd95708ecbff76cfec547ba8
Wilhelm Eschen <wilhelm.eschen@uni-jena.de>
2022-12-18 00:25:55 +0000

    OPR and tv code added, some changes to make it functional still missing

---
+++ PtyLab/Engines/ePIE_TV.py
@@ -0,0 +1,155 @@
+import numpy as np
+from matplotlib import pyplot as plt
+try:
+    import cupy as cp
+except ImportError:
+    print('Cupy not available, will not be able to run GPU based computation')
+    # Still define the name, we'll take care of it later but in this way it's still possible
+    # to see that gPIE exists for example.
+    cp = None
+
+# fracPy imports
+from fracPy.Reconstruction.Reconstruction import Reconstruction
+from fracPy.Engines.BaseEngine import BaseEngine
+from fracPy.ExperimentalData.ExperimentalData import ExperimentalData
+from fracPy.Params.Params import Params
+from fracPy.utils.gpuUtils import getArrayModule
+from fracPy.Monitor.Monitor import Monitor
+from fracPy.utils.utils import fft2c, ifft2c
+import logging
+import tqdm
+import sys
+
+
+class ePIE_TV(BaseEngine):
+
+    def __init__(self, reconstruction: Reconstruction, experimentalData: ExperimentalData, params: Params, monitor: Monitor):
+        # This contains reconstruction parameters that are specific to the reconstruction
+        # but not necessarily to ePIE reconstruction
+        super().__init__(reconstruction, experimentalData, params, monitor)
+        self.logger = logging.getLogger('ePIE')
+        self.logger.info('Sucesfully created ePIE ePIE_engine')
+        self.logger.info('Wavelength attribute: %s', self.reconstruction.wavelength)
+        self.initializeReconstructionParams()
+
+    def initializeReconstructionParams(self):
+        """
+        Set parameters that are specific to the ePIE settings.
+        :return:
+        """
+        self.betaProbe = 0.25
+        self.betaObject = 0.25
+        self.numIterations = 50
+
+
+    def reconstruct(self):
+        self._prepareReconstruction()
+
+        # actual reconstruction ePIE_engine
+        self.pbar = tqdm.trange(self.numIterations, desc='ePIE', file=sys.stdout, leave=True)
+        for loop in self.pbar:
+            # set position order
+            self.setPositionOrder()
+            for positionLoop, positionIndex in enumerate(self.positionIndices):
+                # get object patch
+                row, col = self.reconstruction.positions[positionIndex]
+                sy = slice(row, row + self.reconstruction.Np)
+                sx = slice(col, col + self.reconstruction.Np)
+                # note that object patch has size of probe array
+                objectPatch = self.reconstruction.object[..., sy, sx].copy()
+                
+                # make exit surface wave
+                self.reconstruction.esw = objectPatch * self.reconstruction.probe
+                
+                # propagate to camera, intensityProjection, propagate back to object
+                self.intensityProjection(positionIndex)
+
+                # difference term
+                DELTA = self.reconstruction.eswUpdate - self.reconstruction.esw
+                if loop % 5 == 0:
+                    # object update
+                    self.reconstruction.object[..., sy, sx] = self.objectPatchUpdate_TV(objectPatch, DELTA)
+                else:
+                    self.reconstruction.object[..., sy, sx] = self.objectPatchUpdate(objectPatch, DELTA)
+
+                # probe update
+                # self.reconstruction.probe = self.probeUpdate(objectPatch, DELTA)
+                self.probeUpdate_new(objectPatch, DELTA)
+
+            # get error metric
+            self.getErrorMetrics()
+
+            # apply Constraints
+            self.applyConstraints(loop)
+
+            # show reconstruction
+            self.showReconstruction(loop)
+
+        if self.params.gpuFlag:
+            self.logger.info('switch to cpu')
+            self._move_data_to_cpu()
+            self.params.gpuFlag = 0
+
+
+    def objectPatchUpdate(self, objectPatch: np.ndarray, DELTA: np.ndarray):
+        """
+        Todo add docstring
+        :param objectPatch:
+        :param DELTA:
+        :return:
+        """
+        # find out which array module to use, numpy or cupy (or other...)
+        xp = getArrayModule(objectPatch)
+
+        frac = self.reconstruction.probe.conj() / xp.max(xp.sum(xp.abs(self.reconstruction.probe) ** 2, axis=(0, 1, 2, 3)))
+        return objectPatch + self.betaObject * xp.sum(frac * DELTA, axis=(0,2,3), keepdims=True)
+
+    def objectPatchUpdate_TV(self, objectPatch: np.ndarray, DELTA: np.ndarray):
+        """
+        Todo add docstring
+        :param objectPatch:
+        :param DELTA:
+        :return:
+        """
+        def divergence(f):
+            xp = getArrayModule(f[0])
+            return xp.gradient(f[0], axis=(4, 5))[0] + xp.gradient(f[1], axis=(4, 5))[1]
+
+        xp = getArrayModule(objectPatch)
+        frac = self.reconstruction.probe.conj() / xp.max(xp.sum(xp.abs(self.reconstruction.probe) ** 2, axis=(0, 1, 2, 3)))
+
+        epsilon = 1e-2
+        gradient = xp.gradient(objectPatch, axis=(4, 5))
+        # norm = xp.abs(gradient[0] + gradient[1]) ** 2
+        norm = (gradient[0] + gradient[1]) ** 2
+        temp = [gradient[0] / xp.sqrt(norm + epsilon), gradient[1] / xp.sqrt(norm + epsilon)]
+        TV_update = divergence(temp) 
+        lam = 5e-4
+        return objectPatch + self.betaObject * xp.sum(frac * DELTA, axis=(0,2,3), keepdims=True) + lam * self.betaObject * TV_update
+
+    def probeUpdate(self, objectPatch: np.ndarray, DELTA: np.ndarray):
+        """
+        Todo add docstring
+        :param objectPatch:
+        :param DELTA:
+        :return:
+        """
+        # find out which array module to use, numpy or cupy (or other...)
+        xp = getArrayModule(objectPatch)
+        frac = objectPatch.conj() / xp.max(xp.sum(xp.abs(objectPatch) ** 2, axis=(0,1,2,3)))
+        r = self.reconstruction.probe + self.betaProbe * xp.sum(frac * DELTA, axis=(0, 1, 3), keepdims=True)
+        return r
+    
+    def probeUpdate_new(self, objectPatch: np.ndarray, DELTA: np.ndarray):
+        """
+        Todo add docstring
+        :param objectPatch:
+        :param DELTA:
+        :return:
+        """
+        # find out which array module to use, numpy or cupy (or other...)
+        xp = getArrayModule(objectPatch)
+        # frac = objectPatch.conj() / xp.max(xp.sum(xp.abs(objectPatch) ** 2, axis=(0,1,2,3)))
+        self.reconstruction.probe += self.betaProbe * xp.sum(objectPatch.conj() / xp.max(xp.sum(xp.abs(objectPatch) ** 2, axis=(0,1,2,3))) * DELTA, axis=(0, 1, 3), keepdims=True)
+
+
