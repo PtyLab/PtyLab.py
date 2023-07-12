9e6eba00277886aa001e5dbec83a30975f648e38
Jacob Seifert <40793407+Duxon@users.noreply.github.com>
2023-07-12 18:57:58 +0200

    additional dependencies for run_tests.sh

--- install_linux.sh
+++ install_linux.sh
@@ -1,3 +1,4 @@
  #conda install -c anaconda pytables
- conda install -c conda-forge cupy tqdm
- #conda install matplotlib
+ conda install -c conda-forge cupy tqdm 
+ # additional dependencies required by run_tests.sh:
+ pip install scikit-learn pyqtgraph scikit-image tables
