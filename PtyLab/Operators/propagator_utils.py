20290e1a8d8f00fbc437d827b0424198e47eedcd
ShantanuKodgirwar <shantanukodgirwar@gmail.com>
2024-08-09 19:26:47 +0200

    minor formatting

--- PtyLab/Operators/propagator_utils.py
+++ PtyLab/Operators/propagator_utils.py
@@ -27,6 +27,7 @@ def complexexp(angle):
     xp = getArrayModule(angle)
     return xp.cos(angle) + 1j * xp.sin(angle)
 
+
 def iterate_6d_fields(fields):
     """Efficiently iterating via a generator for the 6D field array
     (nlambda, nosm, npsm, nslice, Np, Np).
@@ -47,16 +48,18 @@ def iterate_6d_fields(fields):
                 for l_nslice in range(fields.shape[3]):
                     yield (i_nlambda, j_nosm, k_npsm, l_nslice)
 
+
 # creating a bandpass filter
 def convolve2d(in1, in2, on_gpu, mode="same"):
-    """ Using the convolve2d function based on whether on GPU of not"""
+    """Using the convolve2d function based on whether on GPU of not"""
     if on_gpu:
         return _fft_convolve2d(in1, in2, on_gpu, mode=mode)
     else:
         return signal.convolve2d(in1, in2, mode=mode)
 
+
 def gaussian2D(n, std, on_gpu):
-    """ Creates a 2D gaussian """
+    """Creates a 2D gaussian"""
     xp = cp if on_gpu else np
     # create the grid of (x,y) values
     n = (n - 1) // 2
@@ -72,29 +75,31 @@ def gaussian2D(n, std, on_gpu):
         h = h / sumh
     return h
 
-def _fft_convolve2d(x, y, on_gpu, mode='same'):
+
+def _fft_convolve2d(x, y, on_gpu, mode="same"):
     """
     2D convolution using FFT, for CuPy arrays.
     """
     xp = cp if on_gpu else np
-    
+
     s1 = x.shape
     s2 = y.shape
     size = s1[0] + s2[0] - 1, s1[1] + s2[1] - 1
     fx = xp.fft.fft2(x, size)
     fy = xp.fft.fft2(y, size)
     result = xp.real(xp.fft.ifft2(fx * fy))
-    
-    if mode == 'same':
+
+    if mode == "same":
         return _centered(result, s1, on_gpu)
-    elif mode == 'valid':
+    elif mode == "valid":
         return _centered(result, (s1[0] - s2[0] + 1, s1[1] - s2[1] + 1))
     else:  # 'full'
         return result
 
+
 def _centered(arr, newsize, on_gpu):
     xp = cp if on_gpu else np
-    
+
     # Return the center newsize portion of the array.
     newsize = xp.asarray(newsize)
     currsize = xp.array(arr.shape)
