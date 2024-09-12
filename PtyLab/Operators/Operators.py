7ddbefbe1d838b4779650a59d741248faaa73b27
Shantanu Kodgirwar <shantanukodgirwar@gmail.com>
2024-09-12 17:20:26 +0200

    adding the right dict

--- PtyLab/Operators/Operators.py
+++ PtyLab/Operators/Operators.py
@@ -13,11 +13,13 @@ except ImportError:
     pass
     # print("Cupy not available, will not be able to run GPU based computation")
 import numpy as np
+
 from PtyLab import Params, Reconstruction
 from PtyLab.Operators._propagation_kernels import __make_quad_phase
 from PtyLab.Operators.off_axis_sas import (
     __make_transferfunction_off_axis_sas,
     propagate_off_axis_sas,
+    propagate_off_axis_sas_inv,
 )
 from PtyLab.Operators.propagator_utils import complexexp
 from PtyLab.utils.gpuUtils import getArrayModule, isGpuArray
@@ -1080,5 +1082,5 @@ reverse_lookup_dictionary = {
     "scaledpolychromeasp": propagate_scaledPolychromeASP_inv,
     "twosteppolychrome": propagate_twoStepPolychrome_inv,
     "identity": propagate_identity,
-    "offaxissas": propagate_off_axis_sas,  # TODO: The inverse operator needs to implemented
+    "offaxissas": propagate_off_axis_sas_inv,
 }
