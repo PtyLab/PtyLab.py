9990ff77d1e006166be9f1cba3ea3e2194120ffd
Wilhelm Eschen <wilhelm.eschen@gmail.com>
2025-03-11 11:02:11 +0100

    e3PIE engine fixed and example added

--- PtyLab/Engines/e3PIE.py
+++ PtyLab/Engines/e3PIE.py
@@ -5,12 +5,13 @@ from matplotlib import pyplot as plt
 try:
     import cupy as cp
 except ImportError:
-    # print("Cupy not available, will not be able to run GPU based computation")
+    # print('Cupy not available, will not be able to run GPU based computation')
     # Still define the name, we'll take care of it later but in this way it's still possible
     # to see that gPIE exists for example.
     cp = None
 
 import logging
+import sys
 
 from PtyLab.Engines.BaseEngine import BaseEngine
 from PtyLab.ExperimentalData.ExperimentalData import ExperimentalData
@@ -18,12 +19,13 @@ from PtyLab.Monitor.Monitor import Monitor
 from PtyLab.Operators.Operators import aspw
 from PtyLab.Params.Params import Params
 
-# PtyLab imports
+# fracPy imports
 from PtyLab.Reconstruction.Reconstruction import Reconstruction
 from PtyLab.utils.gpuUtils import asNumpyArray, getArrayModule
 
 
 class e3PIE(BaseEngine):
+
     def __init__(
         self,
         reconstruction: Reconstruction,
@@ -50,15 +52,31 @@ class e3PIE(BaseEngine):
         self.params.betaObject = 0.25
         self.numIterations = 50
 
-        # preallocate transfer function
-        self.reconstruction.H = aspw(
-            np.squeeze(self.reconstruction.probe[0, 0, 0, 0, ...]),
-            self.reconstruction.dz,
-            self.reconstruction.wavelength / self.reconstruction.refrIndex,
-            self.reconstruction.Lp,
-        )[1]
-        # shift transfer function to avoid fftshifts for FFTS
-        self.reconstruction.H = np.fft.ifftshift(self.optimizableH)
+        if False:
+            # preallocate transfer function
+            self.reconstruction.H = aspw(
+                np.squeeze(self.reconstruction.probe[0, 0, 0, 0, ...]),
+                self.reconstruction.dz,
+                self.reconstruction.wavelength / self.reconstruction.refrIndex,
+                self.reconstruction.Lp,
+            )[1]
+            # shift transfer function to avoid fftshifts for FFTS
+            # self.reconstruction.H = np.fft.ifftshift(self.optimizableH)
+            self.reconstruction.H = np.fft.ifftshift(self.reconstruction.H)
+
+        if True:
+            import cupy as xp
+
+            # preallocate transfer function
+            self.reconstruction.H = aspw(
+                xp.squeeze(self.reconstruction.probe[0, 0, 0, 0, ...]),
+                self.reconstruction.dz,
+                self.reconstruction.wavelength / self.reconstruction.refrIndex,
+                self.reconstruction.Lp,
+            )[1]
+            # shift transfer function to avoid fftshifts for FFTS
+            # self.reconstruction.H = np.fft.ifftshift(self.optimizableH)
+            self.reconstruction.H = xp.fft.ifftshift(self.reconstruction.H)
 
     def reconstruct(self):
         self._prepareReconstruction()
@@ -67,10 +85,15 @@ class e3PIE(BaseEngine):
         self.reconstruction.esw = self.reconstruction.probe.copy()
         # get module
         xp = getArrayModule(self.reconstruction.object)
-        # actual reconstruction e3PIE_engine
-        for loop in tqdm.tqdm(range(self.params.numIterations)):
-            # set position order
-            if loop == self.params.numIterations - 1:
+
+        self.pbar = tqdm.trange(
+            self.numIterations, desc="e3PIE", file=sys.stdout, leave=True
+        )
+
+        # self.pbar = (1, 2)
+
+        for loop in self.pbar:
+            if loop == self.numIterations - 1:
                 noreason = True
             self.setPositionOrder()
             for positionLoop, positionIndex in enumerate(self.positionIndices):
@@ -80,7 +103,8 @@ class e3PIE(BaseEngine):
                 sx = slice(col, col + self.reconstruction.Np)
                 # note that object patch has size of probe array
                 objectPatch = self.reconstruction.object[..., sy, sx].copy()
-                objectPatch2 = self.reconstruction.object[..., :, :].copy()
+                # objectPatch2 = self.reconstruction.object[..., :, :].copy()
+
                 # form first slice esw (exit surface wave)
                 self.reconstruction.esw[:, :, :, 0, ...] = (
                     objectPatch[:, :, :, 0, ...]
@@ -93,7 +117,7 @@ class e3PIE(BaseEngine):
                         xp.fft.fft2(
                             self.reconstruction.esw[:, :, :, sliceLoop - 1, ...]
                         )
-                        * self.H
+                        * self.reconstruction.H
                     )
                     self.reconstruction.esw[:, :, :, sliceLoop, ...] = (
                         self.reconstruction.probe[:, :, :, sliceLoop, ...]
@@ -107,10 +131,13 @@ class e3PIE(BaseEngine):
                 DELTA = (self.reconstruction.eswUpdate - self.reconstruction.esw)[
                     :, :, :, -1, ...
                 ]
-
                 # update object slice
                 for loopTemp in range(self.reconstruction.nslice - 1):
+
                     sliceLoop = self.reconstruction.nslice - 1 - loopTemp
+
+                    # temp_delta = self.reconstruction.esw[..., sliceLoop, sy, sx]
+
                     # compute and update current object slice
                     self.reconstruction.object[..., sliceLoop, sy, sx] = (
                         self.objectPatchUpdate(
@@ -120,7 +147,7 @@ class e3PIE(BaseEngine):
                         )
                     )
                     # eswTemp update (here probe incident on last slice)
-                    beth = 1  # todo, why need beth, not betaProbe, changable?
+                    beth = 0.9 # todo, why need beth, not betaProbe, changable?
                     self.reconstruction.probe[:, :, :, sliceLoop, ...] = (
                         self.probeUpdate(
                             objectPatch[:, :, :, sliceLoop, ...],
@@ -136,7 +163,7 @@ class e3PIE(BaseEngine):
                             xp.fft.fft2(
                                 self.reconstruction.probe[:, :, :, sliceLoop, ...]
                             )
-                            * self.optimizableH.conj()
+                            * self.reconstruction.H.conj()
                         )
                         - self.reconstruction.esw[:, :, :, sliceLoop - 1, ...]
                     )
@@ -152,7 +179,7 @@ class e3PIE(BaseEngine):
                     objectPatch[:, :, :, 0, ...],
                     DELTA,
                     self.reconstruction.probe[:, :, :, 0, ...],
-                    self.params.betaProbe,
+                    self.betaProbe,
                 )
 
             # set porduct of all object slices
@@ -186,7 +213,7 @@ class e3PIE(BaseEngine):
         frac = localProbe.conj() / xp.max(
             xp.sum(xp.abs(localProbe) ** 2, axis=(0, 1, 2))
         )
-        return objectPatch + self.params.betaObject * xp.sum(
+        return objectPatch + self.betaObject * xp.sum(
             frac * DELTA, axis=(0, 2), keepdims=True
         )
 
@@ -204,7 +231,5 @@ class e3PIE(BaseEngine):
         frac = objectPatch.conj() / xp.max(
             xp.sum(xp.abs(objectPatch) ** 2, axis=(0, 1, 2))
         )
-        r = localProbe + self.params.betaProbe * xp.sum(
-            frac * DELTA, axis=(0, 1), keepdims=True
-        )
+        r = localProbe + beth * xp.sum(frac * DELTA, axis=(0, 1), keepdims=True)
         return r
