fb31502c9da82357dd6d564c57c78d5d356b837f
Shantanu Kodgirwar <shantanukodgirwar@gmail.com>
2026-09-21 12:15:39 +0200

    minor test change

--- tests/Monitor/test_center_angle.py
+++ tests/Monitor/test_center_angle.py
@@ -1,8 +1,6 @@
-import pytest
 import matplotlib
 matplotlib.use("Agg")
 import numpy as np
-pytest.importorskip("tensorflow")
 from PtyLab.Monitor.TensorboardMonitor import center_angle
 from scipy import ndimage
 
@@ -10,6 +8,7 @@ from scipy import ndimage
 def test_center_angle():
     N = 128
     Ein = np.fft.fftshift(ndimage.fourier_shift(np.ones((N, N)), [5, 0]))
-    Ein_c = center_angle(Ein)
+    Ein_c, shift = center_angle(Ein)
     assert Ein_c is not None
     assert Ein_c.shape == Ein.shape
+    np.testing.assert_allclose(shift, [5, 0], atol=1e-10)
